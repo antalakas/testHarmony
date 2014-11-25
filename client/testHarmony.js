8ee3810173b745971f87141f1e72f319c5d4fd94
Andreas Ntalakas <antalakas@gmail.com>
2014-11-25 13:45:32 +0000

    First commit

---
+++ client/testHarmony.js
@@ -0,0 +1,20 @@
+// counter starts at 0
+Session.setDefault("counter", 0);
+
+createMonster = function() {
+};
+
+Template.hello.helpers({
+  counter: function () {
+    return Session.get("counter");
+  }
+});
+
+Template.hello.events({
+  'click button': function () {
+    // increment the counter when button is clicked
+    Session.set("counter", Session.get("counter") + 1);
+    
+    //console.log(myMonster);
+  }
+});
